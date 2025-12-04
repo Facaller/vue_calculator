146264b47831e28d396e155884f07d95dae43568
Facaller <ashwin.metal13@gmail.com>
2025-12-04 19:19:59 +0200

    Add some structure, what is syntax :)?

---
+++ calculator.vue
@@ -0,0 +1,36 @@
+<script setup>
+    import {ref} from 'vue';
+
+    const ValueOne = ref(null);
+    const ValueTwo = ref(null);
+    const mathOperator = ref('');
+
+    function add (a, b) {
+        return a + b;
+    }
+
+    function subtract (a, b) {
+        return a - b;
+    }
+
+    function multiply (a, b) {
+        return a * b;
+    }
+
+    function divide (a, b) {
+        return a / b;
+    }
+
+    function operate (a, operator, b) {
+        
+    }
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
